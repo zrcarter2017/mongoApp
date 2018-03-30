286217c9a968de77aa53a9b40e599697dc500e0d
zrcarter2017 <zcarter@allegiancy.us>
2018-03-29 20:53:56 -0400

    fix note title

--- public/app.js
+++ public/app.js
@@ -32,7 +32,7 @@ $(document).on("click", ".scrape", function() {
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<h2>" + 'Take a Note' + "</h2>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
